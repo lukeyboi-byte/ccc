ab80665a6be6434a5070a4ffd8d2dcd5c4629127
Luke <1019817@student.sae.edu.au>
2021-03-25 22:52:11 +1100

    Drag and drop functionality

--- Assets/Scripts/MouseCursor.cs
+++ Assets/Scripts/MouseCursor.cs
@@ -8,29 +8,31 @@ public class MouseCursor : MonoBehaviour
 {
     public Vector3 mousePos;
     public Texture2D cursorArrow;
-        
+
     // Start is called before the first frame update
     void Start()
     {
         Cursor.SetCursor(cursorArrow, mousePos, CursorMode.ForceSoftware);
+        
+        //will only work for the build of the game
+        Cursor.lockState = CursorLockMode.Confined;
     }
 
     // Update is called once per frame
     void Update()
     {
         Mouse();
-        
-        //hello
     }
 
     public void Mouse()
     {
         mousePos = Input.mousePosition;
-        
+
         if (Input.GetMouseButtonDown(0))
         {
             Debug.Log("y= " + mousePos.x);
             Debug.Log("x= " + mousePos.y);
         }
+        
     }
 }
